d98c925527df78bb2e5f0aa755720980aa5f6640
miracle6037 <603763193@qq.com>
2018-05-21 23:03:08 +0800

    Update DateUtil.java

--- DateUtil.java
+++ DateUtil.java
@@ -3,11 +3,10 @@ package com.didispace;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
 public class DateUtil {
-	int offSetHour;
-	int offSetMinute;
-	
+	private static int offSetHour;
+	private static int offSetMinute;
+
 	@org.junit.Test
 	public void test() {
 		String dateStr = "2018-05-21T21:12:00.000-08:00";
@@ -15,30 +14,30 @@ public class DateUtil {
 		System.out.println(parseString2LocalDatetTime(dateStr, "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"));
 		System.out.println(parseString2LocalDatetTime(dateStr1, "yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
 	}
-	
-	public LocalDateTime parseString2LocalDatetTime(String dateStr, String formatter) {
-		String offSet;
-		int lastPlusSign = dateStr.lastIndexOf("+");
-		int countMuinsSign = dateStr.split("-").length;
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(formatter);
-		LocalDateTime formatdDate = LocalDateTime.parse(dateStr, dateTimeFormatter);
 
-		if(lastPlusSign != -1) {
-			offSet = dateStr.substring(lastPlusSign+1, dateStr.length());
+	public static LocalDateTime parseString2LocalDatetTime(String stringDate, String formatter) {
+		String offSet;
+		int lastPlusSignIndex = stringDate.lastIndexOf("+");
+		int countMuinsSign = stringDate.split("-").length;
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(formatter);
+		LocalDateTime formatdDate = LocalDateTime.parse(stringDate, dateTimeFormatter);
+
+		if (lastPlusSignIndex != -1) {
+			offSet = stringDate.substring(lastPlusSignIndex + 1, stringDate.length());
 			getHourAndMinuteFromOffSet(offSet);
 			return formatdDate.minusHours(offSetHour).minusMinutes(offSetMinute);
 		}
 		// 2018/05/21T21:12:00.000-08:00
 		// 2018-05-21T21:12:00.000-08:00
 		if (countMuinsSign == 2 || countMuinsSign == 4) {
-			offSet = dateStr.substring(dateStr.lastIndexOf("-")+1, dateStr.length());
+			offSet = stringDate.substring(stringDate.lastIndexOf("-") + 1, stringDate.length());
 			getHourAndMinuteFromOffSet(offSet);
 			return formatdDate.plusHours(offSetHour).plusMinutes(offSetMinute);
 		}
 		return formatdDate;
 	}
-	
-	public void getHourAndMinuteFromOffSet(String offSet){
+
+	public static void getHourAndMinuteFromOffSet(String offSet) {
 		if (offSet.lastIndexOf(":") != -1) {
 			offSetHour = Integer.parseInt(offSet.split(":")[0]);
 			offSetMinute = Integer.parseInt(offSet.split(":")[1]);
@@ -47,5 +46,5 @@ public class DateUtil {
 			offSetMinute = Integer.parseInt(offSet.substring(2, 4));
 		}
 	}
-	
+
 }
